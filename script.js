eaab166b4c4088d2c813eca85d56fc19452b9f13
Jacob Hargraves <jacobhargraves1@gmail.com>
2023-05-01 11:13:37 -0600

    added event listener and prompts so that prompts start when generate password button is selected

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", criteriaPrompt);
+
+// Variables for prompts
+var promptInitiate = function() {
+  alert("Please select password criteria");
+  return;
+}
+var passwordLengthPrompt = function() {
+  prompt("Please select password length (8-128 characters)");
+  return;
+}
+var lowercase = function() {
+  confirm("Would you like to include lowercase leters?");
+  return;
+}
+var uppercase = function() {
+  confirm("Would you like to include uppercase leters?");
+  return;
+}
+var numbers = function () {
+  confirm("Would you like to include numbers?");
+  return;
+}
+var specialCharacters = function () {
+  confirm("Would you like to include special characters?");
+  return;
+}
+
+// initiate prompts
+function criteriaPrompt() {
+  promptInitiate();
+  passwordLengthPrompt();
+  lowercase();
+  uppercase();
+  numbers();
+  specialCharacters();
+  return;
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +49,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
